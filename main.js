a108d12ee267924b1a495a58caddc532c7e3c72c
Tavnaunidh <76837151+Tavnaunidh@users.noreply.github.com>
2022-04-28 13:01:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function start()
+{
+    navigator.mediaDevices.getUserMedia({ audio: true});
+     classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/51nZg7yI1/', modelReady)
+}
+
